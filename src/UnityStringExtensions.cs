5eb4bcc58b5f702a0b5c2672d717001dfc2fd173
Erik Svedäng <erik.svedang@gmail.com>
2022-03-29 14:30:31 +0200

    fix: TimedSubstring

--- src/UnityStringExtensions.cs
+++ src/UnityStringExtensions.cs
@@ -4,10 +4,10 @@ namespace Jam
 
     public static class StringEx
     {
-        public static string TimedSubstring(this string s, float speed = 2f)
+        public static string TimedSubstring(this string s, float speed = 5f)
         {
             int len = 1 + ((int)(Time.time * speed) % s.Length);
-            return s.Substring(len);
+            return s.Substring(0, len);
         }
     }
 }
