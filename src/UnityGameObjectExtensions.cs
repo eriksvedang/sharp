16a99f6bebf9a45a2cd3f888796de95f2699f591
Erik Svedäng <erik.svedang@gmail.com>
2022-03-30 13:48:32 +0200

    refactor: shorter code

--- src/UnityGameObjectExtensions.cs
+++ src/UnityGameObjectExtensions.cs
@@ -1,28 +1,21 @@
 namespace Jam
 {
     using System;
+    using System.Linq;
     using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.SceneManagement;
 
     public static class GameObjectEx
     {
-        public static IEnumerable<T> FindObjectsBy<T>(Func<T, bool> predicate, Scene? limitToScene = null) where T : Component
+        public static IEnumerable<T> FindObjectsBy<T>(Func<T, bool> predicate) where T : Component
         {
-            var objs = GameObject.FindObjectsOfType<T>();
-
-            foreach (var obj in objs)
-            {
-                if (predicate(obj) && (limitToScene == null || limitToScene == obj.gameObject.scene))
-                {
-                    yield return obj;
-                }
-            }
+            return GameObject.FindObjectsOfType<T>().Where(predicate);
         }
 
-        public static IEnumerable<T> FindObjectsWithinSphere<T>(Vector3 center, float radius, Scene? limitToScene = null) where T : Component
+        public static IEnumerable<T> FindObjectsWithinSphere<T>(Vector3 center, float radius) where T : Component
         {
-            return FindObjectsBy<T>(t => Vector3.Distance(center, t.transform.position) < radius, limitToScene);
+            return FindObjectsBy<T>(t => Vector3.Distance(center, t.transform.position) < radius);
         }
 
         public static IEnumerable<T> InScene<T>(this IEnumerable<T> components, Scene scene) where T : Component
