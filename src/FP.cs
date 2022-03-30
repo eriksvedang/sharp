b27a1dd170bc9d233a708174bc1daf2b4107db9c
Erik Svedäng <erik.svedang@gmail.com>
2022-03-30 14:12:22 +0200

    fix: fp

---
+++ src/FP.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Jam
+{
+    public static class FP
+    {
+        public static T Identity<T>(T x)
+        {
+            return x;
+        }
+    }
+}
